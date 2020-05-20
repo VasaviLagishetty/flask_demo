1cddff673bf6c280c7d8d6a9a2e503560fd7a77b
VasaviLagishetty <47534164+VasaviLagishetty@users.noreply.github.com>
2020-05-20 21:41:43 +0530

    Add files via upload

--- pollution.py
+++ pollution.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,Markup
+from flask import Flask, render_template, request, Markup
 import requests
 import plotly as py
 import plotly.graph_objs as go
@@ -13,15 +13,13 @@ def fetch():
     link = "https://api.thingspeak.com/channels/1019362/feeds.json?api_key=VURU7P85PQ06OZAX&results="
     data_info = requests.get(link)
     data = data_info.json()
-    if no_of_results == '':
-        no_of_results = 6
     print("Json data from URL")
     print(data)
     print("no_of_results",no_of_results)
     no_of_results = int(no_of_results)
     return data,no_of_results
 
-def results():
+def results(with_graph = 1):
     info,no_of_results = fetch()
     total_values = len(info["feeds"])
     if total_values <= no_of_results:
@@ -33,32 +31,33 @@ def results():
     temp = zip(date,value)
     print("date,time : ",date)
     print("Pollution value : ",value)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=date,y=value))
-    fig.layout["font"] = dict(color="#000000",size=16)
-    fig.layout["legend"] = dict(font=dict(color="black", size=12), orientation="h", bgcolor="#b0c4de")
-    fig.layout["paper_bgcolor"] = "#f9f9f9"
-    fig.layout['plot_bgcolor'] = "#f9f9f9"
-    # fig.layout['width']=500  
-    fig.layout.margin={'t':20,'l':20,'b':20,'r':20}  
-    fig.layout.title=fig.layout.title={
-        'text': "Visualization",
-        'y':0.98,
-        'x':0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'}
-    fig.show()
+    if with_graph == 1:
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=date,y=value))
+        fig.layout["font"] = dict(color="#000000",size=16)
+        fig.layout["legend"] = dict(font=dict(color="black", size=12), orientation="h", bgcolor="#b0c4de")
+        fig.layout["paper_bgcolor"] = "#f9f9f9"
+        fig.layout['plot_bgcolor'] = "#f9f9f9"
+        # fig.layout['width']=500  
+        fig.layout.margin={'t':20,'l':20,'b':20,'r':20}  
+        fig.layout.title=fig.layout.title={
+            'text': "Visualization",
+            'y':0.98,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'}
+        fig.show()
     return temp
 
 @app.route('/')
 def home():
-    temp = results()
-    return render_template('pollution.html',temp = temp)
+    data = results(with_graph = 0)
+    return render_template('pollution.html',data = data)
 
 @app.route('/page',methods = ['POST'])
 def fetch_data():
-    temp = results()
-    return render_template('pollution.html',temp = temp)
+    data = results()
+    return render_template('pollution.html',data = data)
 
 if __name__ == '__main__':
    app.run(port = 1234,debug = True)
